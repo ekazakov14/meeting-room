393be796ba0642eabc8606737bda1447cb50ac24
eugene <eugene.kazakov78@gmail.com>
2020-05-22 22:35:48 +0300

    add [minGuestCount, maxGuestCount] to Room type

--- src/models/Room.ts
+++ src/models/Room.ts
@@ -3,6 +3,8 @@ import BasicModel from './BasicModel';
 type Fields = {
   readonly name: string,
   readonly floor: number,
+  readonly minGuestCount: number,
+  readonly maxGuestCount: number,
 };
 
 const Room = BasicModel<Fields>('rooms');
