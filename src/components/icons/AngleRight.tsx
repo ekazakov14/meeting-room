c11825b3fad389dae009046eb5aa45f195b9d2cf
eugene <eugene.kazakov78@gmail.com>
2020-05-24 23:57:50 +0300

    add edit icon, format all icons

--- src/components/icons/AngleRight.tsx
+++ src/components/icons/AngleRight.tsx
@@ -6,17 +6,17 @@ type Props = {
   height: number,
 };
 
-const AngleRight = ({fill, width, height}: Props) => (
+const AngleRight = ({ fill, width, height }: Props) => (
   <svg width={`${width}px`} height={`${height}px`} viewBox="0 0 7 12">
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeOpacity="0.427536232">
-          <g transform="translate(-207.000000, -88.000000)" stroke={fill} stroke-width="2">
-              <g transform="translate(24.000000, 82.000000)">
-                  <g transform="translate(179.000000, 7.000000)">
-                      <polyline transform="translate(5.000000, 5.000000) rotate(-135.000000) translate(-5.000000, -5.000000) " points="8 8 2 8 2 2 2 2"></polyline>
-                  </g>
-              </g>
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeOpacity="0.427536232">
+      <g transform="translate(-207.000000, -88.000000)" stroke={fill} stroke-width="2">
+        <g transform="translate(24.000000, 82.000000)">
+          <g transform="translate(179.000000, 7.000000)">
+            <polyline transform="translate(5.000000, 5.000000) rotate(-135.000000) translate(-5.000000, -5.000000) " points="8 8 2 8 2 2 2 2"></polyline>
           </g>
+        </g>
       </g>
+    </g>
   </svg>
 );
 
